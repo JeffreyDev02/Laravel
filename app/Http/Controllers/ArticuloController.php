6a0e8392c3b5356fa4d6189e30beb7d59c85e0c0
Jeffrey <jeffrey.carino@lumation.co>
2024-05-07 23:08:13 -0600

    third commit

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -42,4 +42,5 @@ class ArticuloController extends Controller
 
     }
 
+
 }
--- resources/views/almacen/articulo/create.blade.php
+++ resources/views/almacen/articulo/create.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6">
-            <h3>Nueva Categoria</h3>
+            <h3>Nueva Articulo</h3>
             @if(count($errors)> 0)
             <div class="alert alert-danger">
                 <ul>
@@ -12,18 +12,38 @@
                 </ul>
             </div>
             @endif
-        </div>
-
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <select class="form-select" aria-label="Default select example">
+            <div class="form-group">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+                <label for="codigo" class="form-label">Codigo</label>
+                <input type="text" name="codigo" id="codigo" class="form-control" placeholder="Codigo">
+            </div>
+            <div class="form-group">
+                <label for="stock" class="form-label">Stock</label>
+                <input type="number" name="stock" id="stock" class="form-control" placeholder="Stock">
+            </div>
+            <div class="form-group">
+                <label for="descripcion" class="form-label">Descripcion</label>
+                <input type="text" name="descripcion" id="descripcion" class="form-control" placeholder="descripcion">
+            </div>
+            <div class="form-group">
+                <label for="idcategoria" class="form-label">Id Categoria</label>
+                <br>
+                <select class="form-select" aria-label="Default select example" name="idcategoria" id="idcategoria">
+                    <option value="" selected disabled>--Eliga una opción</option>    
                     @foreach($categorias as $cat)
-                        <option value="{{$cat -> idcategoria}}">{{$cat -> idcategoria}} - {{$cat -> nombre}}</option>
+                    <option value="{{$cat -> idcategoria}}">{{$cat -> idcategoria}} - {{$cat -> nombre}}</option>
                     @endforeach
                 </select>
             </div>
+            <div class="form-group">
+                <button class="btn btn-primary" type="submit">Guardar</button>
+                <button class="btn btn-danger" type="reset">Cancelar</button>
+            </div>
         </div>
-        
+
 
     </div>
 @endsection
